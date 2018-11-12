8bdd95905986c1c8f6c546ade403743863fbe7b9
Eileen S <eileen.j.sul@gmail.com>
2018-11-12 12:25:46 -0600

    html and js pseudocode

---
+++ app.js
@@ -0,0 +1,15 @@
+// Firebase application to store real time database information
+// Initialize Firebase -- set from false to true in app
+var config = {
+    apiKey: "AIzaSyAJbgIoZsAO5asmIpAB4EQIR2wH4DQtfyc",
+    authDomain: "triviagame-ba643.firebaseapp.com",
+    databaseURL: "https://triviagame-ba643.firebaseio.com",
+    projectId: "triviagame-ba643",
+    storageBucket: "triviagame-ba643.appspot.com",
+    messagingSenderId: "227891755490"
+  };
+  firebase.initializeApp(config);
+// Moment.js app
+// Intial values
+// Capture button click
+// Child added function in order to append table rows
